cdd80f4b8a7845ddc757f789ed06dbbd8713626a
Austine Huang <austine.huang@gmail.com>
2025-12-13 16:59:46 +0800

    Initial Commit

---
+++ pearticleHabits/pearticleHabitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  pearticleHabitsApp.swift
+//  pearticleHabits
+//
+//  Created by Austine Huang on 2025/12/13.
+//
+
+import SwiftUI
+
+@main
+struct pearticleHabitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
